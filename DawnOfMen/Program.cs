614a3e81eb7af492c026f12d75eea18ff7249214
Admin <Admin@DESKTOP-AOF506V>
2023-02-06 16:43:13 +0530

    Add project files.

---
+++ DawnOfMen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DawnOfMen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("demmm");
+        }
+    }
+}
---
+++ Lab1/Meth2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Meth2
+    {
+        public static int gcd(int num1,int num2)
+        {
+            while (num2 > 0)
+            {
+                int r = num1%num2;
+                num1 = num2;
+                num2 = r;
+            }
+            return num1;
+        }
+        public static int lcm(int num1,int num2)
+        {
+            return (num1/gcd(num1,num2))*num2;
+        }
+        static void Main(string[] args)
+        {
+            int number;
+            int[] divisors = { 2, 3, 4, 5, 6 };
+            int displayNumberIndex = 1;
+            int lcmOfDivisors = 1;
+            foreach(int divisior in divisors)
+            {
+                lcmOfDivisors = lcm(lcmOfDivisors,divisior);
+            }
+            for (number = 1; displayNumberIndex < 5; number += lcmOfDivisors)
+            {
+                if (number % 7 == 0)
+                {
+                    if (displayNumberIndex != 3)
+                    {
+                        Console.WriteLine(number);
+                    }
+                    displayNumberIndex++;
+                }
+            }
+        }
+    }
+}
